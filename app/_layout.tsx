5be1bbc281d0663c0d4293cf4d075ec320bf7cf0
Muhammad Furqan Abbas <mfurqanabbas20@gmail.com>
2025-12-28 11:12:27 +0500

    feat: add route guard to redirect user to login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+
+
+function RouteGuard({children}: {children: React.ReactNode}){
+  const router = useRouter();
+  const isAuth = false;
+
+  useEffect(() => {
+    if(!isAuth){
+      router.replace('/auth')
+    }
+  }, [])
+
+  return <>{children}</>
+}
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+     
+    <Stack screenOptions={{headerShown: false}}>
+      <RouteGuard>
+        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+      </RouteGuard>
     </Stack>
   )
 }
