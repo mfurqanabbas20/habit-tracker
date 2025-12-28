18f54ef94fe7c14ae0c74557f1fcfa8343a73e47
Muhammad Furqan Abbas <mfurqanabbas20@gmail.com>
2025-12-28 11:13:13 +0500

    feat: add basic tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,17 @@
 import { Tabs } from "expo-router";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function TabsLayout() {
   return (
     // tabBarActiveTintColor
     <Tabs screenOptions={{tabBarActiveBackgroundColor: "coral"}}>
-      <Tabs.Screen name="index" options={{title: "Home"}} />
+      <Tabs.Screen name="index" options={{title: "Home", tabBarIcon: ({color, focused}) => {
+        return focused
+          ?
+         <FontAwesome5 name="about" />
+          :
+          <FontAwesome5 name="home" color={color} />
+        }}}  />
       <Tabs.Screen name="login" options={{title: "Login"}} />
     </Tabs>
   )
