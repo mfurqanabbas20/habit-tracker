067891133bf0e610cbc8c666dfa3a4c736e3374a
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-01-03 20:58:07 +0500

    feat: add habit interface

--- lib/types/habits.ts
+++ lib/types/habits.ts
@@ -1,5 +1,17 @@
+import { Models } from "react-native-appwrite";
+
 export interface DataType {
     title: string;
     description: string;
     frequency: string;
+}
+
+export interface Habit extends Models.Row {
+    userId: string;
+    title: string;
+    description: string;
+    frequency: string;
+    streak_count: number;
+    last_completed: string;
+    created_at: string;
 }
