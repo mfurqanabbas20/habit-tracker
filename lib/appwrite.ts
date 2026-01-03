f3cc1dc483492b67953b63cf009acd75c2060f1a
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-01-03 13:18:38 +0500

    fix: add table db object

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,5 @@
 // install reactnattive appwrite sdk
-import {Account, Client, Databases} from "react-native-appwrite";
+import {Account, Client, Databases, TablesDB} from "react-native-appwrite";
 
 export const client = new Client()
   .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
@@ -7,6 +7,7 @@ export const client = new Client()
 
 
 export const account = new Account(client);
+export const tableDatabases = new TablesDB(client);
 export const databases = new Databases(client);
 
 export const DATABASE_ID = process.env.EXPO_PUBLIC_DATABASE_ID;
