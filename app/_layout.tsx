4c2bc07a82c3c2cc6d3161f02926b719677e1ada
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-01-08 23:10:32 +0500

    fix: minor indent

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function RouteGuard({children}: {children: React.ReactNode}){
   const router = useRouter();
@@ -20,7 +20,9 @@ function RouteGuard({children}: {children: React.ReactNode}){
     }
   }, [user, segments])
 
-  return <>{children}</>
+  return <>
+  {children}
+  </>
 }
 
 export default function RootLayout() {
